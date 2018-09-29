caf44449df511255d8e445d7eecacb4dbb85f6ce
Your Name <dp.15cs10@thdcihet.ac.in>
2018-09-29 21:47:03 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ function myFunction(){ 
+
+                if(document.forms['frm'].answer.value == "" )
+                {
+                    alert("empty title");
+                }
+               if(document.forms['frm'].description.value == ""){
+					alert("empty description");
+                  }
+                
+                return;
+            }
---
+++ main1.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="colguess.js"></script>
